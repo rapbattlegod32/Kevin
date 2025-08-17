4fa7bc671c3622a4ce03ee246aa82489000ab984
alex <70163067+rapbattlegod32@users.noreply.github.com>
2025-08-17 08:39:01 +0100

    kevin

--- src/main/java/com/alex/kevin/listeners/EventListener.java
+++ src/main/java/com/alex/kevin/listeners/EventListener.java
@@ -1,35 +1,28 @@
 package com.alex.kevin.listeners;
 
 import com.alex.kevin.config.Secrets;
-import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.io.File;
 
 public class EventListener extends ListenerAdapter {
     Secrets config = new Secrets();
-
-    @Override
-    public void onMessageReactionAdd(MessageReactionAddEvent event) {
-        User user = event.getUser();
-        String emoji = event.getReaction().getEmoji().getAsReactionCode();
-        String channelMention = event.getChannel().getAsMention();
-        String jumpLink = event.getJumpUrl();
-
-        String message = user.getAsTag() + " | " + user.getId() + " reacted to a message with:\nemoji: " + emoji + "\nchannel: " + channelMention
-                + "\njump: " + jumpLink;
-        event.getGuild().getDefaultChannel().asTextChannel().sendMessage(message).queue();
-
-    }
+    final File folder = new File("C:\\Users\\Alexu\\Desktop\\kevin\\src\\main\\resources\\media\\schieste");
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         String content = event.getMessage().getContentRaw().toLowerCase();
         char prefix = config.getPrefix();
 
-        if (content.equals(prefix + "ping")){
-            String message = "Pong!";
-            event.getMessage().reply(message).queue();
+        if (content.contains("kevin")){
+            kevin(event);
         }
     }
+
+    public void kevin(MessageReceivedEvent event){
+        String message = "Hey " + event.getAuthor().getAsMention();
+        event.getMessage().reply(message).queue();
+    }
+
 }
