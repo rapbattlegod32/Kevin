a0ce657b38cb85b081a812a85531bae8ad858c01
alex <70163067+rapbattlegod32@users.noreply.github.com>
2025-08-17 08:40:24 +0100

    cache policys and chunkingfilter

--- src/main/java/com/alex/kevin/Kevin.java
+++ src/main/java/com/alex/kevin/Kevin.java
@@ -1,5 +1,6 @@
 package com.alex.kevin;
 
+import com.alex.kevin.commands.CommandManager;
 import com.alex.kevin.config.Secrets;
 import com.alex.kevin.listeners.EventListener;
 import net.dv8tion.jda.api.OnlineStatus;
@@ -7,6 +8,8 @@ import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
 
 public class Kevin {
     static Secrets config = new Secrets();
@@ -17,11 +20,13 @@ public class Kevin {
         DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(config.getToken());
         builder.setStatus(OnlineStatus.ONLINE);
         builder.setActivity(Activity.listening("something"));
-        builder.enableIntents(GatewayIntent.MESSAGE_CONTENT);
+        builder.enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS);
+        builder.setMemberCachePolicy(MemberCachePolicy.ALL);
+        builder.setChunkingFilter(ChunkingFilter.ALL);
         shardManager = builder.build();
         
         // register listeners
-        shardManager.addEventListener(new EventListener());
+        shardManager.addEventListener(new EventListener(), new CommandManager());
     }
 
     public ShardManager getShardManager() {
